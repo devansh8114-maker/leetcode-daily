cedb3be8569ff225cfd542f556263847453a4c04
devansh8114-maker <devansh8114@gmail.com>
2026-07-10 22:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (15.50%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& str) {
-        reverse(str.begin(),str.end());
+        int st=0;
+        int end=str.size()-1;
+
+        while(st<end){
+            swap(str[st],str[end]);
+            st++;
+            end--;
+        }
     }
 };
